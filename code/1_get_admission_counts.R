1893da2ae24d3049a0dbb0efb5d2dbdea88db359
Emma Thomas <emmathomas@g.harvard.edu>
2019-11-21 18:35:58 -0500

    Extracted counts by zip code for MH/neuro outcomes

--- code/1_get_admission_counts.R
+++ code/1_get_admission_counts.R
@@ -1,7 +1,7 @@
 # Code to get admission counts by person, by year for medicare data
 
 # Make sure working directory is set to moretrees2/code
-# setwd("/nfs/home/E/ethomas/shared_space/ci3_analysis/moretrees2/code/")
+setwd("/nfs/home/E/ethomas/shared_space/ci3_analysis/moretrees2/code/")
 
 library(NSAPHutils)
 set_threads()
@@ -17,18 +17,35 @@ codes <- c(expand_range(chaps[[1]][1], chaps[[1]][2]),
 codes <- get_leaf(codes)
 
 admissions <- "../data/admissions"
+denom_path <- "../data/denominator/"
+
+denom_columns <- c("qid", "zip", "pm25_ensemble")
 
 for (year_ in 2000:2014) {
   admission_data <- read_data(admissions, years = year_, columns = c("QID", "DIAG1"))
   names(admission_data) <- tolower(names(admission_data))
   
   # Keep only relevant diagnoses
-  admission_data <- subset(admission_data, diag1 %in% codes
+  admission_data <- subset(admission_data, diag1 %in% codes)
   admission_data$diag1 <- factor(admission_data$diag1, levels = codes)
   
-  # This creates new columns for each ICD9 code with counts of every time each Medicare
-  # beneficiary was hospitalized with that code as principal diagnosis
-  admission_data <- dcast(admission_data, qid ~ diag1, fun.aggregate = length, drop = F)
+  # Get zip codes
+  patient_summary <- read_data(denom_path, years = year_, columns = denom_columns)
+  
+  # Merge in zip codes
+  admission_data <- merge(admission_data, patient_summary, by = "qid", all.x = T)
+  
+  # This creates new columns for each ICD9 codes with counts of principal diagnosis by zip
+  admission_data$zip <- factor(admission_data$zip, levels = unique(patient_summary$zip))
+  admission_data <- dcast(admission_data, zip ~ diag1, fun.aggregate = length, drop = F) 
+  
+  # Merge in denominator/pm25 data
+  counts <- patient_summary[ , .(denom = .N, pm25 = unique(pm25_ensemble)), by = zip]
+  counts$zip <- factor(counts$zip, levels = unique(patient_summary$zip))
+  counts <- merge(counts, admission_data, by = "zip", all.x = T)
+  # note: NA zip codes in admissions data get dropped here
+  
+  # print(summary(colSums(counts[ , ..codes])))
   
   # This creates annual counts of how many hospitalizations for each individual occured
   # The .SD notation is a bit confusing so please ask me about it
@@ -36,6 +53,11 @@ for (year_ in 2000:2014) {
   # admission_data <- admission_data[,lapply(.SD, sum, na.rm = T), by = "qid",
   #                                  .SDcols = codes]
   # admission_data[, year := year_]
-  
-  write_fst(admission_data, paste0("../data/admission_counts/admission_counts_", year_, ".fst"))
+  write_fst(counts, paste0("../data/admission_counts/admission_counts_", year_, ".fst"))
+}
+
+for (year_ in 2000:2014) {
+  counts <- read_fst(paste0("../data/admission_counts/admission_counts_", year_, ".fst"))
+  print(year_)
+  print(summary(colSums(counts[ , codes])))
 }
--- code/2_merge_admission_counts.R
+++ code/2_merge_admission_counts.R
@@ -2,15 +2,24 @@
 library(NSAPHutils)
 set_threads()
 
+# Make sure working directory is set to moretrees2/code
+# setwd("/nfs/home/E/ethomas/shared_space/ci3_analysis/moretrees2/code/")
+
 library(data.table)
 library(fst)
 library(icd)
 
-denominator_columns <- c("zip", "year", "qid", "race", "sex", "age",
-                         "dual", "dead", "pm25_ensemble", "tmmx",
-                         "whatever else you think you need")
-admission_columns <- c("qid", "condition1", "condition2", "whatever")
-conditions
+# Get list of relevant ICD9 codes at billable level
+chaps <- icd9_chapters[c("Mental Disorders", "Diseases Of The Nervous System And Sense Organs")]
+codes <- c(expand_range(chaps[[1]][1], chaps[[1]][2]), 
+           expand_range(chaps[[2]][1], chaps[[2]][2]))
+codes <- get_leaf(codes)
+
+denominator_columns <- c("zip", "year", "qid")
+                         #"race", "sex", "age",
+                         #"dual", "dead", "pm25_ensemble", "tmmx",
+                         #"whatever else you think you need")
+admission_columns <- c("qid", codes)
 
 denom_path <- "../data/denominator/"
 admission_counts <- "../data/admission_counts/"
@@ -20,7 +29,10 @@ for (year_ in 2000:2014) {
                                columns = denominator_columns)
   admissions <- read_data(admission_counts, years = year_, 
                           columns = admission_columns)
-  merged <- merge(patient_summary, admissions, all.x = T, by = "qid")
+  merged <- merge(patient_summary, admissions, all.y = T, by = "qid")
+  
+  # aggregate by zip code
+  denom <- dcast(patient_summary, zip ~ year_, fun.aggregate = length, drop = F)
   
   # memory management, don't need the other data in memory any more
   rm(patient_summary)
