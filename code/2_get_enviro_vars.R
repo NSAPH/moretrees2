22641a3ec57d422e0dee10894aec5221bde274b2
Emma Thomas <emmathomas@g.harvard.edu>
2020-02-03 14:31:21 -0500

    First attempt at code to process temperature data

--- code/2_get_enviro_vars.R
+++ code/2_get_enviro_vars.R
@@ -1,7 +1,6 @@
 require(readr)
 require(fst)
 require(magrittr)
-require(reshape2)
 require(data.table)
 
 setwd("/nfs/home/E/ethomas/shared_space/ci3_analysis/moretrees2/code/")
@@ -14,57 +13,62 @@ zipReverse <- function(x) as.character(x) %>%
   as.numeric
 
 # ------------------------------------------------------ PM2.5 ------------------------------------------------
+
 pm <- fread("../data/daily_pm/all_days_PM.csv", data.table = T,
             key = c("ZIP", "date"))
-
-# Temperature
-# temp <- fread("../data/temperature/temperature_daily_zipcode_combined.csv",
-#              nrow = 10)
+# re-format date variable
+pm[ , date := as.Date(date, format = "%Y-%m-%d")]
+# Keep only relevant years
+pm <- pm[date >= as.Date("2000-01-01") & 
+               date <= as.Date("2014-12-31")]
 
 for (year_ in 2000:2014) {
   # subset to particular year
-  pm_year <- pm[date <= year_ * 10000 + 9999 & 
-                      date >= year_ * 10000 - 1]
+  pm_year <- pm[date <= as.Date(paste0(year_, "-12-31")) &
+                  date >= as.Date(paste0(year_, "-01-01")) - 2] # need to include two days due to lag
   # reverse zip codes (???)
   pm_year[ , ZIP := zipReverse(ZIP)]
-  # re-format date variable
-  pm_year$date <- as.character(pm_year$date) %>% 
-    as.Date(format = "%Y%m%d")
   # sort by zip/date
   pm_year <- pm_year[order(ZIP, date)]
   # compute lag01 PM2.5
   pm_year[ , c("pm25_lag1", "pm25_lag2") := shift(pm25, n = 1:2, type = "lag"),
                            by = ZIP]
+  # exclude extra lag days
+  pm_year <- pm_year[date >= as.Date(paste0(year_, "-01-01"))]
   # write to file
   write_fst(pm_year, path = paste0("../data/enviro/pm_", year_, ".fst"))
   # remove subsetted data.table for memory purposes
   rm(pm_year)
 }
+# remove large PM dataset
+rm(pm)
 
 # ----------------------------------------------- Temperature ------------------------------------------------
 
 # Temperature
-# temp <- fread("../data/temperature/temperature_daily_zipcode_combined.csv",
-#              nrow = 10)
+temp <- fread("../data/temperature/temperature_daily_zipcode_combined.csv", data.table = T,
+                 key = c("ZIP", "date"))
+temp[ , c("pr", "year") := NULL]
+temp[ , date := as.Date(date, format = "%Y-%m-%d")]
+temp <- temp[date >= as.Date("2000-01-01") & 
+               date <= as.Date("2014-12-31")]
 
 for (year_ in 2000:2014) {
   # subset to particular year
-  temp_year <- temp[date <= year_ * 10000 + 9999 & 
-                  date >= year_ * 10000 - 1]
-  # reverse zip codes (???)
-  temp_year[ , ZIP := zipReverse(ZIP)]
-  # re-format date variable
-  temp_year$date <- as.character(pm_year$date) %>% 
-    as.Date(format = "%Y%m%d")
+  temp_year <- temp[date <= as.Date(paste0(year_, "-12-31")) &
+                      date >= as.Date(paste0(year_, "-01-01"))]
+  # # reverse zip codes (???)
+  # temp_year[ , ZIP := zipReverse(ZIP)]
   # sort by zip/date
-  pm_year <- pm_year[order(ZIP, date)]
-  # compute lag01 PM2.5
-  pm_year[ , c("pm25_lag1", "pm25_lag2") := shift(pm25, n = 1:2, type = "lag"),
-          by = ZIP]
+  temp_year <- temp_year[order(ZIP, date)]
   # write to file
-  write_fst(pm_year, file = paste0("../data/enviro/pm_", year_, ".fst"))
+  write_fst(temp_year, path = paste0("../data/enviro/temp_", year_, ".fst"))
   # remove subsetted data.table for memory purposes
-  rm(pm_year)
+  rm(temp_year)
 }
+# remove large temp dataset
+rm(temp)
+
+# ----------------------------------------------- Ozone ---------------------------------------------------
 
 # ozone <- read_rds("../data/ozone_data/PREDICTIONGeneral2_OZONE_zipcode_SUBSET_2000_2012.rds")
