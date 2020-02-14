82247222175edfe72590a03a782c67f38cc2d18d
Emma Thomas <emmathomas@g.harvard.edu>
2020-02-14 13:36:06 -0500

    Merged admissions and enviro date; did time-stratified sampling of controls

--- code/2_get_enviro_vars.R
+++ code/2_get_enviro_vars.R
@@ -6,21 +6,22 @@ require(lubridate)
 
 setwd("/nfs/home/E/ethomas/shared_space/ci3_analysis/moretrees2/code/")
 
+# Relevant dates
+control_dist <- 0 # max number of days between case day and control day
+maxlag <- 3 # maximum lag to consider
+buffer <- control_dist + maxlag # total buffer
+year_range <- 2000:2014 # years to include
+
 # ------------------------------------------------------ PM2.5 ------------------------------------------------
 
 pm <- fread("../data/daily_pm/all_days_PM.csv", data.table = T,
             key = c("ZIP", "date"))
 # re-format date variable
 pm[ , date := ymd(date)]
-# Keep only relevant years 
-# (need to keep buffer of one week + 3 days on either end due to lag and control days)
-control_dist <- 7 # max number of days between case day and control day
-maxlag <- 3 # maximum lag to consider
-buffer <- control_dist + maxlag
 pm <- pm[date >= make_date(year = 2000, month = 1, day = 1) - buffer & 
                date <= make_date(year = 2014, month = 12, day = 31) + buffer]
 
-for (year_ in 2000:2014) {
+for (year_ in year_range) {
   # subset to particular year
   # (need to keep buffer of one week + 3 days on either end due to lag and control days)
   pm_year <- pm[date >= make_date(year = year_, month = 1, day = 1) - buffer &
@@ -53,7 +54,7 @@ temp[ , date := ymd(date)]
 temp <- temp[date >= make_date(year = 2000, month = 1, day = 1) - buffer & 
                date <= make_date(year = 2014, month = 12, day = 31) + buffer]
 
-for (year_ in 2000:2014) {
+for (year_ in year_range) {
   # subset to particular year
   # (need to keep buffer of one week + 3 days on either end due to lag and control days)
   temp_year <- temp[date >= make_date(year = year_, month = 1, day = 1) - buffer &
@@ -80,6 +81,41 @@ rm(temp)
 
 # ----------------------------------------------- Ozone ---------------------------------------------------
 
-# ozone <- read_rds("../data/ozone_data/PREDICTIONGeneral2_OZONE_zipcode_SUBSET_2000_2012.rds")
+for (year_ in year_range) {
+  # get relevant dates
+  dates <- seq(make_date(year = year_, month = 1, day = 1) - buffer,
+                 make_date(year = year_, month = 12, day = 31) + buffer,
+               by = "days")
+  dates <- format(dates, "%Y%m%d")
+  ozone_year <- data.table(ZIP = integer(0),
+                           ozone = numeric(0),
+                           date = integer(0))
+  # read in ozone data by days
+  for (date_ in dates) {
+    date_file <- paste0("../data/ozone/", date_, ".csv")
+    if (file.exists(date_file)) {
+      ozone_date <- fread(file = date_file, data.table = T)
+      ozone_year <- rbind(ozone_year, ozone_date)
+    }
+  }
+  
+  # format date variable
+  ozone_year[ , date := ymd(date)]
+  
+  # sort by zip/date
+  setkey(ozone_year, ZIP, date)
+  ozone_year <- ozone_year[order(ZIP, date)]
+  # compute lags 1, 2 for PM2.5
+  ozone_year[ , c("ozone_lag1", "ozone_lag2") := shift(ozone, n = 1:2, type = "lag"),
+          by = ZIP]
+  # exclude extra lag days
+  ozone_year <- ozone_year[date >= make_date(year = year_, month = 1, day = 1) - control_dist &
+                       date <= make_date(year = year_, month = 12, day = 31) + control_dist]
+  # write to file
+  write_fst(ozone_year, path = paste0("../data/enviro/ozone_", year_, ".fst"))
+  # remove subsetted data.table for memory purposes
+  rm(ozone_year)
+  print(year_)
+}
 
 
--- code/3_merge_admissions_enviro.R
+++ code/3_merge_admissions_enviro.R
@@ -12,23 +12,46 @@ library(stringr)
 library(readr)
 
 admissions_path <- "../data/admissions_cvd/"
-enviro_path <- "../data/temperature/"
+enviro_path <- "../data/enviro/"
 merged_path <- "../data/merged_admissions_enviro/"
+
+# function that gets potential control dates for all case dates in a range
+get_control_dates <- function(begin, end) {
+  dates <- seq(begin, end, by = "day")
+  controls_dt <- data.table(dates = dates, control_days = rep(list(), length(dates)),
+                            key = "dates")
+  for (i in 1:nrow(controls_dt)) {
+    date <- controls_dt$dates[i]
+    start <- make_date(day = 1, month = month(date), year = year(date))
+    control_days <- seq(start, start + days_in_month(start) - 1, by = "day") # match on month/year
+    control_days <- control_days[wday(control_days) == wday(date)] # match on day of week
+    control_days <- control_days[control_days != date] # remove date itself
+    controls_dt$control_days[[i]] <- control_days
+  }
+  return(controls_dt)
+}
+
+admissions_all <- NULL
 
 # set seed because we will randomly select control day
 set.seed(6357312)
-
-admissions_all <- NULL
 
 for (year_ in 2000:2014) {
   # read in admissions
   # NOTE: for now I am ignoring the fact that multiple hospitalizations may occur for same individual
   admissions <- read_fst(paste0("../data/admissions_cvd/admissions_cvd_", year_, ".fst"),
-                        as.data.table = T, columns = c("zip", "adate", "ccs"))
+                        as.data.table = T, columns = c("id", "zip", "adate", "ccs"))
   
-  # select control days (randomly exactly one week before or after)
-  # NOTE: could later swap this to "time-stratified" design
-  admissions$cdate <- admissions$adate + sample(c(-7, 7), nrow(admissions), replace = T)
+  # select control days (stratified on year, month, day of week)
+  # get list of potential controls
+  potential_controls <- get_control_dates(begin = make_date(day = 1, month = 1, year = year_),
+                                          end = make_date(day = 31, month = 12, year = year_))
+  admissions <- merge(admissions, potential_controls, by.x = "adate", by.y = "dates",
+                      all.x = T, all.y = F)
+  # randomly select one control per case
+  admissions[ , cdate := lapply(control_days, sample, size = 1)]
+  admissions[ , cdate := do.call("c", cdate)]
+  admissions[ , control_days := NULL]
   
   # read in PM2.5 data
   pm25 <- read_fst(paste0("../data/enviro/pm_", year_, ".fst"), 
@@ -65,11 +88,29 @@ for (year_ in 2000:2014) {
   admissions <- merge(admissions, temp, by.x = c("zip", "cdate"), by.y = c("ZIP", "date"),
                       all.x = T, all.y = F)
   
-  # Remove temp data
+  # remove temp data
   rm(temp)
+
+  # read in ozone data
+  ozone <- read_fst(paste0("../data/enviro/ozone_", year_, ".fst"), as.data.table = T,
+			columns = c("ZIP", "date", "ozone", "ozone_lag1"))
+
+  # Compute lag01 ozone
+  ozone[ , ozone_lag01_case := (ozone + ozone_lag1) / 2]
+  ozone[ , c("ozone", "ozone_lag1") := NULL]
+
+  # merge ozone data for case and control days
+  admissions <- merge(admissions, ozone, by.x = c("zip", "adate"), by.y = c("ZIP", "date"),
+		all.x = T, all.y = F)
+  setnames(ozone, "ozone_lag01_case", "ozone_lag01_control")
+  admissions <- merge(admissions, ozone, by.x = c("zip", "cdate"), by.y = c("ZIP", "date"),
+		all.x = T, all.y = F)
+
+  # Remove ozone data
+  rm(ozone)
   
   # Drop unnecessary variables
-  admissions[ , c("adate", "cdate", "zip") := NULL]
+  # admissions[ , c("adate", "cdate", "zip") := NULL]
   
   # concatenate with other year's data
   admissions_all <- rbind(admissions_all, admissions)
@@ -81,5 +122,5 @@ for (year_ in 2000:2014) {
   print(year_)
 }
 
-# write to file
+# write to file (all admissions)
 write_fst(admissions_all, "../data/merged_admissions_enviro/admissions_enviro.fst")
