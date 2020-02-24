57c179629383f78613fbe23e806680b1abe3f1ac
Emma Thomas <emmathomas@g.harvard.edu>
2020-02-24 15:19:16 -0800

    Minor edits

--- code/6_figures_and_tables.R
+++ code/6_figures_and_tables.R
@@ -1,11 +1,15 @@
 # Read in results of moretrees model
 load(file = "./results/attempt1.Rdata")
+moretrees_results_init <- moretrees_results
+load(file = "./results/attempt1_run2.Rdata")
 
 # Plot ELBO
 ELBO <- moretrees_results$mod$ELBO_track
-plot.start <- 30
+plot.start <- 7600
 plot.end <- length(ELBO)
+# plot.end <- 8000
 plot(plot.start:plot.end, ELBO[plot.start:plot.end], type = "l")
+ELBO[plot.end] - ELBO[plot.end - 1]
 
 # Create results table
 OR_est <- exp(moretrees_results$beta_moretrees[ , 2:4] * 10)
