23be3bf8ec540aa18439594bada628446a5586af
Emma Thomas <emmathomas@g.harvard.edu>
2020-03-03 17:57:42 -0800

    Minor edits

--- code/6_figures_and_tables.R
+++ code/6_figures_and_tables.R
@@ -3,7 +3,7 @@
 source("./code/results_functions.R")
 
 # Read in results of moretrees model 
-load(file = "./results/mod1_split35_northEast.Rdata")
+load(file = "./results/mod1_split35_northEast_cvd.Rdata")
 
 # Create results table 
 OR_est <- ccs_table(root = "7", moretrees_results = moretrees_results,
@@ -16,8 +16,8 @@ OR_xtable <- xtable(OR_est, align = c("l", "l", "p{7.2cm}", "r", "r", "p{2.2cm}"
                     digits = 3, display = c("d", "d", "s", "d", "f", "f", "f"))
 names(OR_xtable) <- c("Group", "CCS codes", 
                       "$n_{out}$", "$n_{obs}$",
-                      "RR below $35 \\mu g \cdot m^{-3}$ (95\\%CI)",
-                      "RR above $35 \\mu g \cdot m^{-3}$ (95\\%CI)")
+                      "RR below $35 \\mu g \\cdot m^{-3}$ (95\\%CI)",
+                      "RR above $35 \\mu g \\cdot m^{-3}$ (95\\%CI)")
 
 write(print(OR_xtable, floating = FALSE, include.rownames = FALSE,
             sanitize.text.function = function(x) x),
@@ -35,8 +35,8 @@ row.names(OR_est) <- NULL
 OR_xtable <- xtable(OR_est, align = c("l", "l", "l", "l"), 
                     digits = 3, display = c("d", "d", "f", "f"))
 names(OR_xtable) <- c("Group", 
-                      "RR below $35 \\mu g \cdot m^{-3}$ (95\\%CI)",
-                      "RR above $35 \\mu g \cdot m^{-3}$ (95\\%CI)")
+                      "RR below $35 \\mu g \\cdot m^{-3}$ (95\\%CI)",
+                      "RR above $35 \\mu g \\cdot m^{-3}$ (95\\%CI)")
 
 write(print(OR_xtable, floating = FALSE, include.rownames = FALSE,
             sanitize.text.function = function(x) x),
@@ -44,6 +44,55 @@ write(print(OR_xtable, floating = FALSE, include.rownames = FALSE,
 
 # Create results tree plot 
 pdf(file = "./figures/mod1_cvd_tree.pdf", width = 6, height = 2.8)
+ccs_plot(root = "7", moretrees_results = moretrees_results,
+         asp = 1/10, leaf.height = 30, label.dist = 4.5)
+dev.off()
+
+# CVD mod2 -------------------------------------------------------------------
+source("./code/results_functions.R")
+
+# Read in results of moretrees model 
+load(file = "./results/mod2_split35_northEast_cvd_attempt2.RData")
+
+# Create results table 
+OR_est <- ccs_table(root = "7", moretrees_results = moretrees_results,
+                    digits = 3, mult = 10)
+OR_est$long_label <- NULL
+OR_est$n_obs <- formatC(OR_est$n_obs, format="d", big.mark=",")
+require(xtable)
+row.names(OR_est) <- NULL
+OR_xtable <- xtable(OR_est, align = c("l", "l", "p{7.2cm}", "r", "r", "p{2.2cm}", "p{2.2cm}"), 
+                    digits = 3, display = c("d", "d", "s", "d", "f", "f", "f"))
+names(OR_xtable) <- c("Group", "CCS codes", 
+                      "$n_{out}$", "$n_{obs}$",
+                      "RR below $35 \\mu g \\cdot m^{-3}$ (95\\%CI)",
+                      "RR above $35 \\mu g \\cdot m^{-3}$ (95\\%CI)")
+
+write(print(OR_xtable, floating = FALSE, include.rownames = FALSE,
+            sanitize.text.function = function(x) x),
+      file = "./figures/table_cvd_mod2.tex")
+
+# Create results table with ML estimates 
+OR_est <- ccs_table(root = "7", moretrees_results = moretrees_results,
+                    type = "ml",
+                    digits = 3, mult = 10)
+OR_est$short_label <- NULL
+OR_est$long_label <- NULL
+OR_est$n_outcomes <- NULL
+OR_est$n_obs <- NULL
+row.names(OR_est) <- NULL
+OR_xtable <- xtable(OR_est, align = c("l", "l", "l", "l"), 
+                    digits = 3, display = c("d", "d", "f", "f"))
+names(OR_xtable) <- c("Group", 
+                      "RR below $35 \\mu g \\cdot m^{-3}$ (95\\%CI)",
+                      "RR above $35 \\mu g \\cdot m^{-3}$ (95\\%CI)")
+
+write(print(OR_xtable, floating = FALSE, include.rownames = FALSE,
+            sanitize.text.function = function(x) x),
+      file = "./figures/table_cvd_mod2_ml.tex")
+
+# Create results tree plot 
+pdf(file = "./figures/mod2_cvd_tree.pdf", width = 6, height = 2.8)
 ccs_plot(root = "7", moretrees_results = moretrees_results,
          asp = 1/10, leaf.height = 30, label.dist = 4.5)
 dev.off()
@@ -73,8 +122,8 @@ OR_xtable <- xtable(OR_est,align = c("l", "l", "p{7.6cm}", "r", "r", "p{2.2cm}",
                     digits = 3, display = c("d", "d", "s", "d", "d", "f", "f"))
 names(OR_xtable) <- c("Group", "CCS codes", 
                       "$n_{out}$", "$n_{obs}$",
-                      "RR below $35 \\mu g \cdot m^{-3}$ (95\\%CI)",
-                      "RR above $35 \\mu g \cdot m^{-3}$ (95\\%CI)")
+                      "RR below $35 \\mu g \\cdot m^{-3}$ (95\\%CI)",
+                      "RR above $35 \\mu g \\cdot m^{-3}$ (95\\%CI)")
 
 write(print(OR_xtable, floating = FALSE, include.rownames = FALSE,
             sanitize.text.function = function(x) x),
@@ -92,8 +141,8 @@ row.names(OR_est) <- NULL
 OR_xtable <- xtable(OR_est, align = c("l", "l", "l", "l"), 
                     digits = 3, display = c("d", "d", "f", "f"))
 names(OR_xtable) <- c("Group", 
-                      "RR below $35 \\mu g \cdot m^{-3}$ (95\\%CI)",
-                      "RR above $35 \\mu g \cdot m^{-3}$ (95\\%CI)")
+                      "RR below $35 \\mu g \\cdot m^{-3}$ (95\\%CI)",
+                      "RR above $35 \\mu g \\cdot m^{-3}$ (95\\%CI)")
 
 write(print(OR_xtable, floating = FALSE, include.rownames = FALSE,
             sanitize.text.function = function(x) x),
