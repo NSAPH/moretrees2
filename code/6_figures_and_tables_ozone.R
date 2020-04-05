b1dc4c3d2772c18749f721b294962246f9703dc6
Emma Thomas <emmathomas@g.harvard.edu>
2020-04-04 17:23:32 -0700

    Began producing figures and tables for sensitivity analyses

--- code/6_figures_and_tables_ozone.R
+++ code/6_figures_and_tables_ozone.R
@@ -9,21 +9,21 @@ dataset <- c("cvd", "resp")
 root <- c("7", "8")
 splits <- c("0", "25")
 nmods <- 3
-approx_ml <- array(dim = c(length(dataset), length(splits), nmods), 
+approx_ml <- array(dim = c(length(dataset), length(splits), 2), 
                    dimnames = list(dataset = c("Cardiovascular Data", "Respiratory Data"), 
                                    split = splits, 
-                                   Model = 1:nmods))
+                                   Model = 2:nmods))
 
 for (i in 1:length(dataset)) { # datasets
    ds <- dataset[i]
-   for (mod in 1:nmods) { # models
+   for (mod in 2:nmods) { # models
       
       # Read in results of moretrees model 
-      load(file = paste0("./results/mod", mod, "_split0_", ds, ".Rdata"))
-      approx_ml[i, 1, mod] <- moretrees_results$mod$hyperparams$ELBO
+      load(file = paste0("./results/mod", mod, "_split0_", ds, "_ozone.Rdata"))
+      approx_ml[i, 1, mod - 1] <- moretrees_results$mod$hyperparams$ELBO
       mod0 <- moretrees_results
-      load(file = paste0("./results/mod", mod, "_split25_", ds, ".Rdata"))
-      approx_ml[i, 2, mod] <- moretrees_results$mod$hyperparams$ELBO
+      load(file = paste0("./results/mod", mod, "_split25_", ds, "_ozone.Rdata"))
+      approx_ml[i, 2, mod - 1] <- moretrees_results$mod$hyperparams$ELBO
       mod25 <- moretrees_results
       
       # Get tree
@@ -61,58 +61,58 @@ for (i in 1:length(dataset)) { # datasets
                            digits = 1, display = display)
       names(est_xtable) <- tabnames
       
-      tabfile <- paste0("./figures/mod", mod, "_", ds, "_table.tex")
+      tabfile <- paste0("./figures/mod", mod, "_", ds, "_table_ozone.tex")
       write(print(est_xtable, floating = FALSE, include.rownames = FALSE,
                   sanitize.text.function = function(x) x),
             file = tabfile)
       
-      # Create results table with ML estimates
-      est_ml0 <- ccs_table(root = root[i], moretrees_results = mod0,
-                           type = "ml",
-                           digits = 1, mult = 10)
-      est_ml0$short_label <- NULL
-      est_ml0$long_label <- NULL
-      est_ml0$n_outcomes <- NULL
-      est_ml0$n_obs <- NULL
-      row.names(est_ml0) <- NULL
-      est_ml0$ci_est2 <- est_ml0$ci_est1
-      est_ml0 <- cbind("Model" = 1, est_ml0)
-      est_ml25 <- ccs_table(root = root[i], moretrees_results = mod25,
-                            type = "ml",
-                            digits = 1, mult = 10)
-      est_ml25$short_label <- NULL
-      est_ml25$long_label <- NULL
-      est_ml25$n_outcomes <- NULL
-      est_ml25$n_obs <- NULL
-      row.names(est_ml0) <- NULL
-      est_ml25 <- cbind("Model" = 2, est_ml25)
-      est_ml <- rbind(est_ml0, est_ml25)
-      align <- c("l", "l", "l", rep("r", 2))
-      display <- c("d", "d", "d", rep("f", 2))
-      est_xtable_ml <- xtable(est_ml, align = align,
-                              digits = 3, display = display)
-      names(est_xtable_ml) <- tabnames[-c(3, 4, 5)]
-      tabfile <- paste0("./figures/mod", mod, "_", ds, "_ml_table.tex")
-      write(print(est_xtable_ml, floating = FALSE, include.rownames = FALSE,
-                  sanitize.text.function = function(x) x),
-            file = tabfile)
-      
-      # Create matrix plots
-      rownames.lab.offset <- 18.8 * (i == 1) + 19.1 * (i == 2)
-      pltfile2 <- paste0("./figures/mod", mod, "_split0_", ds, "_matrix.pdf")
-      pdf(file = pltfile2, width = 12, height = 9.5)
-      print(equal_betas_plot(prob = mod0$mod$vi_params$prob,
-                             groups = mod0$beta_est$group,
-                             tr = tr,
-                             rownames.lab.offset = rownames.lab.offset))
-      dev.off()
-      pltfile2 <- paste0("./figures/mod", mod, "_split25_", ds, "_matrix.pdf")
-      pdf(file = pltfile2, width = 12, height = 9.5)
-      print(equal_betas_plot(prob = mod25$mod$vi_params$prob,
-                             groups = mod25$beta_est$group,
-                             tr = tr,
-                             rownames.lab.offset = rownames.lab.offset))
-      dev.off()
+      # # Create results table with ML estimates
+      # est_ml0 <- ccs_table(root = root[i], moretrees_results = mod0,
+      #                      type = "ml",
+      #                      digits = 1, mult = 10)
+      # est_ml0$short_label <- NULL
+      # est_ml0$long_label <- NULL
+      # est_ml0$n_outcomes <- NULL
+      # est_ml0$n_obs <- NULL
+      # row.names(est_ml0) <- NULL
+      # est_ml0$ci_est2 <- est_ml0$ci_est1
+      # est_ml0 <- cbind("Model" = 1, est_ml0)
+      # est_ml25 <- ccs_table(root = root[i], moretrees_results = mod25,
+      #                       type = "ml",
+      #                       digits = 1, mult = 10)
+      # est_ml25$short_label <- NULL
+      # est_ml25$long_label <- NULL
+      # est_ml25$n_outcomes <- NULL
+      # est_ml25$n_obs <- NULL
+      # row.names(est_ml0) <- NULL
+      # est_ml25 <- cbind("Model" = 2, est_ml25)
+      # est_ml <- rbind(est_ml0, est_ml25)
+      # align <- c("l", "l", "l", rep("r", 2))
+      # display <- c("d", "d", "d", rep("f", 2))
+      # est_xtable_ml <- xtable(est_ml, align = align,
+      #                         digits = 3, display = display)
+      # names(est_xtable_ml) <- tabnames[-c(3, 4, 5)]
+      # tabfile <- paste0("./figures/mod", mod, "_", ds, "_ml_table.tex")
+      # write(print(est_xtable_ml, floating = FALSE, include.rownames = FALSE,
+      #             sanitize.text.function = function(x) x),
+      #       file = tabfile)
+      # 
+      # # Create matrix plots
+      # rownames.lab.offset <- 18.8 * (i == 1) + 19.1 * (i == 2)
+      # pltfile2 <- paste0("./figures/mod", mod, "_split0_", ds, "_matrix.pdf")
+      # pdf(file = pltfile2, width = 12, height = 9.5)
+      # print(equal_betas_plot(prob = mod0$mod$vi_params$prob,
+      #                        groups = mod0$beta_est$group,
+      #                        tr = tr,
+      #                        rownames.lab.offset = rownames.lab.offset))
+      # dev.off()
+      # pltfile2 <- paste0("./figures/mod", mod, "_split25_", ds, "_matrix.pdf")
+      # pdf(file = pltfile2, width = 12, height = 9.5)
+      # print(equal_betas_plot(prob = mod25$mod$vi_params$prob,
+      #                        groups = mod25$beta_est$group,
+      #                        tr = tr,
+      #                        rownames.lab.offset = rownames.lab.offset))
+      # dev.off()
    }
 }
 
