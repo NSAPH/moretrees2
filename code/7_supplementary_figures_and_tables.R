5d969c017ee4517d80c3468df1ed31faa82f29d1
Emma Thomas <emmathomas@g.harvard.edu>
2020-06-30 09:56:35 -0700

    Tidying up code

--- code/7_supplementary_figures_and_tables.R
+++ code/7_supplementary_figures_and_tables.R
@@ -13,7 +13,7 @@ tr <- induced_subgraph(tr, vids)
 # Get labels
 ccs_labels <- read.csv("./data/Multi_Level_CCS_2015_cleaned/dxm_label_clean.csv")
 
-# Make horrible plot
+# Make plot
 root <- names(igraph::V(tr))[igraph::degree(tr, mode = "in") == 0]
 V(tr)$levels <- as.numeric(igraph::distances(tr, v = root, to = V(tr), mode = "out") + 1)
 labels <- data.frame(node = names(V(tr)),
@@ -81,7 +81,7 @@ tr <- induced_subgraph(tr, vids)
 # Get labels
 ccs_labels <- read.csv("./data/Multi_Level_CCS_2015_cleaned/dxm_label_clean.csv")
 
-# Make horrible plot
+# Make plot
 root <- names(igraph::V(tr))[igraph::degree(tr, mode = "in") == 0]
 V(tr)$levels <- as.numeric(igraph::distances(tr, v = root, to = V(tr), mode = "out") + 1)
 labels <- data.frame(node = names(V(tr)),
@@ -252,3 +252,51 @@ for (j in 1:3) {
     dev.off()
   }
 }
+
+############ Nested plots #############
+
+source("./code/results_functions.R")
+require(fst)
+require(splines)
+dataset <- c("cvd", "resp")
+root <- c("7", "8")
+mod <- 3
+split <- "0"
+
+nknots <- 2
+q <- seq(0, 1, length.out = nknots + 2)
+
+for (i in 1:length(dataset)) { # datasets
+  ds <- dataset[i]
+  
+  load(paste0("./results/nest_pltdat_", ds, ".RData"))
+  pltdat_ml[ , c("est1", "cil1", "ciu1")] <- 100 * (exp(pltdat_ml[, c("est1", "cil1", "ciu1")] * 10 ) - 1)
+  
+  # plot
+  xlab <- "Excess Rate (%)"
+  if (i == 1) {
+    lab.widths <- c(0.75, 0.75, 0.75, 0.85)
+    lab.txt.width <- c(20, 25, 25, 15)
+    pdf(file = "./figures/cvd_nested_clr.pdf", height = 15, width = 14)
+    beta_indiv_plot_fun(pltdat_ml, tr, xlab = xlab, lab.widths = lab.widths,
+                        lab.txt.width = lab.txt.width, axis.height = 1.5,
+                        cil_min = -8, ciu_max = 8,
+                        force_lims = TRUE,
+                        lab.txt.size = 4, digits = 0, axis.txt.size = 10,
+                        plot_depth = 4, wrap_labs = FALSE)
+    dev.off()
+  }
+  if (i == 2) {
+    lab.widths <- c(0.75, 0.75, 0.75, 0.75)
+    lab.txt.width <- c(20, 25, 25, 15)
+    pdf(file = "./figures/resp_nested_clr.pdf", height = 10, width = 14)
+    beta_indiv_plot_fun(pltdat_ml, tr, xlab = xlab, lab.widths = lab.widths,
+                        lab.txt.width = lab.txt.width, axis.height = 1.3,
+                        cil_min = -8, ciu_max = 8,
+                        force_lims = TRUE,
+                        lab.txt.size = 4, digits = 0, axis.txt.size = 10,
+                        plot_depth = 4, wrap_labs = FALSE)
+    dev.off()
+  }
+}
+
